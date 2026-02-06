a276cd12be383c6f1eee5fe27f8fa8caf773c34d
govind <govind.g.coder@gmail.com>
2026-02-06 09:32:14 +0530

    initial commit

--- src/CharFrequency.java
+++ src/CharFrequency.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class CharFrequency {
     
-        // TODO: Read the string S (Use sc.nextLine() to capture spaces)
-        // TODO: Read the character C
+        public static void main(String[] args){
+            //to read string S and C
+            Scanner sc = new Scanner(System.in);
+            String S = sc.nextLine();
+            char C = sc.next().charAt(0);
+            sc.close();
+            S = S.toLowerCase();
+            C = Character.toLowerCase(C);
+            int count = 0;
+            for(int i = 0; i<S.length(); i++){
+                if(C==S.charAt(i)) count++;
+            }
+            System.out.print(count);
+        }
+        //Read the character C
         // TODO: Iterate through the string and count occurrences of C
         // TODO: Print the count
 
