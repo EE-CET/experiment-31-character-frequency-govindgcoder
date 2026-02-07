b41139b73947003f77a6bb308b608eb818f9d8b0
govind <govind.g.coder@gmail.com>
2026-02-07 19:38:38 +0530

    removed lowercase conversion

--- src/CharFrequency.java
+++ src/CharFrequency.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
 
 public class CharFrequency {
-    
-        public static void main(String[] args){
-            //to read string S and C
-            Scanner sc = new Scanner(System.in);
-            String S = sc.nextLine();
-            char C = sc.next().charAt(0);
-            sc.close();
-            S = S.toLowerCase();
-            C = Character.toLowerCase(C);
-            int count = 0;
-            for(int i = 0; i<S.length(); i++){
-                if(C==S.charAt(i)) count++;
-            }
-            System.out.print(count);
-        }
-        //Read the character C
-        // TODO: Iterate through the string and count occurrences of C
-        // TODO: Print the count
 
+    public static void main(String[] args) {
+        //to read string S and C
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        char C = sc.next().charAt(0);
+        sc.close();
+        int count = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if (C == S.charAt(i)) count++;
+        }
+        System.out.print(count);
+    }
+    //Read the character C
+    // TODO: Iterate through the string and count occurrences of C
+    // TODO: Print the count
 }
